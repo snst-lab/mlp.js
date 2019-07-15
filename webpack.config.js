0a08c0c7ba6fc860b423ea758525167327f705bd
snst-lab <snst.lab@gmail.com>
2019-07-15 23:25:11 +0900

    fix webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = [
+	{
+		mode: process.env.NODE_ENV || 'development', // 'production'
+		devtool: 'inline-source-map',
+		entry: {
+			'mlp.min': './src/mlp.js'
+		},
+		output: {
+			path: __dirname,
+			filename: 'public/dist/[name].js'
+		},
+		resolve: {
+			extensions: [ '.js' ]
+		},
+		module: {
+			rules: [
+			  {
+			    test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: [
+				  {
+					loader: 'babel-loader',
+					options: {
+					  presets: [
+						[
+						  "@babel/preset-env",
+						  {
+							targets: {
+							  ie: 11
+							},
+							corejs: 3,
+							useBuiltIns: 'usage'
+						  }
+						]
+					  ]
+					}
+				  }
+				]
+			  }
+			]
+		  }
+	}
+];
